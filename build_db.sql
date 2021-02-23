58cd396a68cde6f79b39a9df3413e1428c0af40e
Devin Brady <devinbrady@gmail.com>
2021-02-23 17:25:58 -0500

    First field test

--- build_db.sql
+++ build_db.sql
@@ -1,26 +1,31 @@
 -- build_db.sql
 
 
-drop table if exists obs;
-
-create table obs (
-    obs_id uuid primary key
-    , session_id uuid
-    , distance_miles float
-    , start_time timestamp
-    , end_time timestamp
-    , elapsed_seconds float
-    , mph float
-);
-
 
 drop table if exists sessions;
 
-create table sessions (
-    session_id uuid primary key
-    , session_mode varchar
-    , full_name varchar
-    , email varchar
-    , distance_miles float
-    , created_at timestamp
-    );
+CREATE TABLE sessions (
+    session_id uuid PRIMARY KEY,
+    session_mode character varying(20),
+    full_name character varying(80),
+    email character varying(120),
+    speed_limit_mph double precision,
+    distance_miles double precision,
+    created_at timestamp without time zone
+);
+
+-- CREATE UNIQUE INDEX sessions_pkey ON sessions(session_id uuid_ops);
+
+
+drop table if exists observations;
+
+CREATE TABLE observations (
+    observation_id uuid PRIMARY KEY,
+    session_id uuid REFERENCES sessions(session_id),
+    valid boolean NOT NULL,
+    start_time timestamp without time zone,
+    end_time timestamp without time zone,
+    elapsed_seconds double precision
+);
+
+-- CREATE UNIQUE INDEX observations_pkey ON observations(observation_id uuid_ops);
