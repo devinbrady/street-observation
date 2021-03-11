4163120f6933904ccc79e1b447f8de707b83d61a
Devin Brady <devinbrady@gmail.com>
2021-03-11 09:56:40 -0500

    Handle case when only one emoji has been observed

--- speed/counter.py
+++ speed/counter.py
@@ -21,8 +21,7 @@ def counter_handler():
     location_id = request.args.get('location_id')
 
     with open(os.path.join(app.root_path, 'queries/emoji_list.sql'), 'r') as f:
-        emoji_list = pd.read_sql(text(f.read()), db.session.bind)
-
+        available_emoji = pd.read_sql(text(f.read()), db.session.bind)
 
     with open(os.path.join(app.root_path, 'queries/emoji_observations.sql'), 'r') as f:
         emoji_observations = pd.read_sql(text(f.read()), db.session.bind, params={'session_id': session_id})
@@ -39,14 +38,29 @@ def counter_handler():
             )
 
         # Left join to the list of all emoji, to show zero for those not yet seen
-        emoji_count = pd.merge(emoji_list, emoji_observation_count, how='left', on='emoji_id').sort_values(by='display_order')
+        emoji_count = pd.merge(available_emoji, emoji_observation_count, how='left', on='emoji_id').sort_values(by='display_order')
         emoji_count['num_observations'] = emoji_count['num_observations'].fillna(0).astype(int)
 
-    else:
-        # No emoji observed yet
+        if len(emoji_observations) == 1:
+            # Only one emoji has been observed, the session has no duration yet, so the per-hour calculation can't be done
+            emoji_count['observations_per_hour'] = 0
 
-        emoji_count = emoji_list.copy()
+        else:
+            # Calculate the duration of this session so far
+            earliest_time = emoji_observations['created_at'].min()
+            latest_time = emoji_observations['created_at'].max()
+            session_duration_hours = (latest_time - earliest_time).total_seconds() / 3600
+
+            # Calculate the number of emoji per hour
+            emoji_count['observations_per_hour'] = emoji_count['num_observations'] / session_duration_hours
+
+
+    else:
+        # Zero emoji have been observed
+
+        emoji_count = available_emoji.copy()
         emoji_count['num_observations'] = 0
+        emoji_count['observations_per_hour'] = 0
 
         emoji_observations = pd.DataFrame()
 
